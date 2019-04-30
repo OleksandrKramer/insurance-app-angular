b427602ae83538832b78fdfb1fecc667ca3eb6d1
OleksandrKramer <oleksandr.kramer@gmail.com>
2019-04-30 13:05:41 +0300

    Created Services with Business Logic.

---
+++ src/app/service/agent.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Agent} from '../model/agent';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgentService {
+
+  baseUrl = 'http://localhost:8080/api/agent';
+
+  constructor(private http: HttpClient) { }
+
+  getAgents() {
+    return this.http.get(`${this.baseUrl}/list`);
+  }
+
+  add(agent: Agent) {
+    return this.http.post(`${this.baseUrl}/add`, agent);
+  }
+
+  update(agent: Agent) {
+    return this.http.post(`${this.baseUrl}/update`, agent);
+  }
+
+  delete(id: any) {
+    return this.http.get(`${this.baseUrl}/delete/${id}`);
+  }
+
+  find(field: string, value: string) {
+    return this.http.get(`${this.baseUrl}/find/${field}/${value}`);
+  }
+}
---
+++ src/app/service/filiation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Filiation} from '../model/filiation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FiliationService {
+
+  baseUrl = 'http://localhost:8080/api/filiation';
+
+  constructor(private http: HttpClient) { }
+
+
+  getFiliations() {
+    return this.http.get(`${this.baseUrl}/list`);
+  }
+
+  addFiliation(filiation: Filiation) {
+    return this.http.post(`${this.baseUrl}/add`, filiation);
+  }
+
+  updateFiliation(filiation: Filiation) {
+    return this.http.post(`${this.baseUrl}/update`, filiation);
+  }
+
+  deleteFiliation(id: any) {
+    return this.http.get(`${this.baseUrl}/delete/${id}`);
+  }
+
+  find(field: string, value: string) {
+    return this.http.get(`${this.baseUrl}/find/${field}/${value}`);
+  }
+
+
+}
